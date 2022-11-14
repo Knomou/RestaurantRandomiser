6d27199c32b37eac0c41438fb8579d1875674ec9
Knomou <108348480+Knomou@users.noreply.github.com>
2022-11-14 23:14:30 +1100

    Slight changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const Restaurants = [
+//List of restaurants
+let restaurants = new Array
+(
     "Gyusha",
     "Musou",
     "Kogi",
@@ -14,16 +16,24 @@ const Restaurants = [
     "Masuya Suisan",
     "Koi",
     "Laduree"
-]
+);
+
+// //Randomises and outputs restaurant
+// let btn = document.getElementById("randomiseButton");
+// function randomise() { restaurants[Math.floor(Math.random() * restaurants.length)] };
+// //outputs restaurant
+// button.addEventListener("click", event => {
+// randomise()
+// });
+
 
 //Pushes out message via button click
-
 const element = document.getElementById("RandomiseButton");
 document.addEventListener("click", getRanRest);
 
 function getRanRest() {    
     // window.confirm(Restaurants[~~(Math.random() * Restaurants.length)]);
-    document.getElementById("printRest").innerHTML = (Restaurants[~~(Math.random() * Restaurants.length)]);
+    document.getElementById("printRest").innerHTML = (restaurants[~~(Math.random() * restaurants.length)]);
 }
 
 // //Get user input
